ab1f5d0ca37dbcf99defa1a3c8219efbf510f65d
Akith-002 <chandinubaa.22@uom.lk>
2024-05-08 18:30:04 +0530

    backend-frontend integration

--- app.vue
+++ app.vue
@@ -1,10 +1,42 @@
-<script setup lang="ts">
-const version = 2 + 1;
-</script>
-
 <template>
-  <div class="hello">Hello Nuxt {{ version }}!</div>
+  <h1>{{ name }}</h1>
+
+  <div v-if="pending">Loading Projects...</div>
+  <div>
+    <h2>Projects</h2>
+    <ul>
+      <li v-for="project in projects" :key="project.name">
+        <h3>{{ project.name }}</h3>
+        <p>{{ project.description }}</p>
+      </li>
+    </ul>
+  </div>
+
+  <div v-if="pending1">Loading Blogs...</div>
+  <div>
+    <h2>Blogs</h2>
+    <ul>
+      <li v-for="blog in blogs">
+        <h3>{{ blog.title }}</h3>
+        <p>{{ blog.content }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script setup>
+const name = "Akith Chandinu";
+const {
+  data: projects,
+  pending,
+  error,
+} = useFetch("http://localhost:5000/projects");
+const {
+  data: blogs,
+  pending1,
+  error1,
+} = useFetch("http://localhost:5000/blogs");
+</script>
 
 <style scoped>
 .hello {
